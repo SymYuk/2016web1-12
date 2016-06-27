13da420bb7560272b86cb242ec22fcc6fc5e3fdc
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-27 18:15:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var input=parseInt(window.prompt("FizzBuzzしよう！回数を入力"));
 if(input<1)
-	document.writeln('Error!  自然数を入力してみよう');
+	document.write('Error!  自然数を入力してみよう');
 else
 	for(var i = 1 ; i <= input ; i++)
 	{
 		if(i%15==0)
-			document.writeln("FizzBuzz,");
+			document.write("FizzBuzz,");
 		else if(i%5==0)
-			document.writeln("Buzz,");
+			document.write("Buzz,");
 		else if(i%3 == 0)
-			document.writeln("Fizz,");
+			document.write("Fizz,");
 		else
-			document.writeln(i+",");
+			document.write("<p>"+i+"</p>");
 	}
