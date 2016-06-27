7842a7b8962004d5be44a69f5839965e7cf8e7d7
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-27 17:45:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ else
 {
 	for(var i = 1 ; i <= input ; i++)
 	{
-		if(i%3 == 0)
-			output += "Fizz\n";
+		if(i%15==0)
+			output += "FizzBuzz\n";
 		else if(i%5==0)
 			output += "Buzz\n";
-		else if(i%15==0)
-			output += "FizzBuzz\n";
+		else if(i%3 == 0)
+			output += "Fizz\n";
 		else
 			output += i+"\n";
 	}
