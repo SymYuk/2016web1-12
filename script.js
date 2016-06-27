43f9dc5d063bb4e2f269425d0774e9b6c304e732
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-27 17:29:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ var output='';
 if(input<1)
 {
 	document.getElementById('FizzBuzz').textContent='Error!\n自然数を入力してみよう';
-	return 0;
+	return;
 }
 
-for(var i=1;i<=input;i++)
-{
-	if(i%3==0)
-	{output += 'Fizz\n';}
-	else if(i%5==0)
-	{output += 'Buzz\n';}
-	else if(i%15==0)
-	{output += 'FizzBuzz\n';}
-	else
-	{output += 'i\n';}
-}
 
-document.getElementById('FizzBuzz').textContent=output;
+	for(var i=1;i<=input;i++)
+	{
+		if(i%3==0)
+		{output += 'Fizz\n';}
+		else if(i%5==0)
+		{output += 'Buzz\n';}
+		else if(i%15==0)
+		{output += 'FizzBuzz\n';}
+		else
+		{output += 'i\n';}
+	}
+	document.getElementById('FizzBuzz').textContent=output;
