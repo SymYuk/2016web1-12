15b53d83083fc44c44b5e2d2c94ad8eb5789fc9c
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-27 17:19:30 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var input=parseInt(window.prompt('FizzBuzzしよう！回数を入力');
+var output='';
+
+if(input<1)
+{
+	document.GetElementById('FizzBuzz').textContent='Error! 自然数を入力してみよう';
+	return;
+}
+
+for(var i=1;i<=input;i++)
+{
+	var li=Document.createElement('FizzBuzz')
+	if(i%3==0)
+	output += 'Fizz';
+	else if(i%5==0)
+	output += 'Buzz';
+	else if(i%15==0)
+	output += 'FizzBuzz';
+	else
+	output += 'i';
+}
+
+document.getElementById('FizzBuzz').textContent=output;
